7601b8eba788b96624644ea2709d6014b00e749f
Aayush Sharma <aayush240805@gmail.com>
2026-03-20 14:37:19 +0530

    done

---
+++ src/main/java/net/engineeringdigest/journalApp/constants/PlaceHolders.java
@@ -0,0 +1,7 @@
+package net.engineeringdigest.journalApp.constants;
+
+
+public interface PlaceHolders {
+    public String API_KEY = "<api_key>";
+    public String CITY = "<city>";
+}
---
+++ src/test/java/net/engineeringdigest/journalApp/service/EmailServiceTest.java
@@ -0,0 +1,20 @@
+package net.engineeringdigest.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTest {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    public void EmailTest() {
+        emailService.sendEmail(
+                "ritu53649@gmail.com",
+                "Message",
+                "Hi");
+    }
+}
