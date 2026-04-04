12ec1aafc7238a3a11a034c7fccffe9b8be210e8
Aayush Sharma <aayush240805@gmail.com>
2026-04-04 16:22:57 +0530

    frontend

---
+++ src/main/java/net/engineeringdigest/journalApp/config/CORS_Config.java
@@ -0,0 +1,17 @@
+package net.engineeringdigest.journalApp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORS_Config implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://front-end-1fxo.onrender.com")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
